fa631c524d5a9781c78ada951b05aa6f5b4a88ed
Rizal Setya Perdana <rizalespe@gmail.com>
2020-02-09 22:44:36 +0900

    menambahkan class menggunakan pretrain

--- klasifikasi-gambar-sederhana/model.py
+++ klasifikasi-gambar-sederhana/model.py
@@ -1,28 +1,46 @@
 #Import needed packages
 import torch
 import torch.nn as nn
+import torchvision.models as models
+
+class CustomPretrain(nn.Module):
+    def __init__(self, num_class):
+        super(CustomPretrain, self).__init__()
+        resnet = models.resnet18(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad = False
+        
+        resnet.fc = nn.Linear(resnet.fc.in_features, num_class)        
+        self.resnet = resnet
+
+    def forward(self, image):        
+        # image = image.view(-1, )
+        out = self.resnet(image)
+        return out
 
 class CustomModel(nn.Module):
     def __init__(self, num_classes=10):
         super(CustomModel, self).__init__()
+
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+                                        nn.Conv2d(3, 16, kernel_size=5, stride=1, padding=2),
+                                        nn.BatchNorm2d(16),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=2, stride=2)
+                                   )
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+                                        nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+                                        nn.BatchNorm2d(32),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=2, stride=2)
+                                    )
         
-        self.fc = nn.Linear(8*8*32, num_classes)
+        self.fc = nn.Linear(32*56*56, num_classes)
         
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
-        
         return out
